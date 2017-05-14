e7ac17fa4dc6daff1f5fdf594ddf659983f8b7d8
charlieoduk <odukjr@gmail.com>
2017-05-14 20:05:54 +0300

    add pseudo code for create room

---
+++ tests/version_0_tests.py
@@ -0,0 +1,17 @@
+import unittest
+from dojo import Dojo
+
+
+class TestCreateRoom(unittest.TestCase):
+
+
+# check if room is created
+
+# check if created toom is an instance of class Room
+
+# Test if maximum capacity of the office is 6
+
+# Test if maximum capacity of the living space is 4
+
+# Test if you can reate mutiple rooms
+
