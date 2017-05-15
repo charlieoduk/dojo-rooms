41c22dd0598293bd95361e2e468552cdb7b243c0
charlieoduk <odukjr@gmail.com>
2017-05-16 01:53:04 +0300

    complete create_room method

--- app/dojo.py
+++ app/dojo.py
@@ -2,32 +2,42 @@ from office import Office
 from livingspace import LivingSpace
 
 class Dojo(object):
-	"""docstring for Dojo"""
-	dojo_offices = []
-	dojo_livingspaces = []
-	def create_room(self, room_type, *room_name):
-		# if isinstance(room_type, str) and isinstance(room_name, str)
-		if isinstance(room_type, str) and isinstance(room_name, str):
-			# if room type is office
-			if room_type == 'office':
-				# create an office
-					# loop through range in room name to create multiple rooms
-					for room in range room_name:
-						new_office = Office(room)
-						dojo_offices.append(new_office)
-			# if room is living space
-			if room_type == 'livingspace':
-				# create living space
-					# loop through range in living space to create multiple rooms
-					for room in range room_name:
-						new_livingspace = LivingSpace(room)
-						dojo_livingspaces.append(new_livingspace)
-			# else
-		    else:
-				# invalid room
-				print('You have entered an invalid room')
-		# else raise value error 
-	    else:
-	    	raise ValueError
+    """docstring for Dojo"""
+    dojo_offices = []
+    dojo_livingspaces = []
 
-		
+    
+    def create_room(room_type, *room_name):        
+        # if isinstance(room_type, str) and isinstance(room_name, str)
+        if type(room_type) == str and type(room_name) == str:
+            # if room type is office
+            if room_type == 'office':
+                # check if room exists
+                if room_type not in dojo_offices:
+                    # loop through range in room name to create multiple rooms
+                    for room in room_name:
+                        new_office = Office(room)
+                        dojo_offices.append(new_office)
+                        print('A new office called {} has been created'.format(room))
+                else:
+                    print('The room {} already exists'.format(room_name))
+            # if room is living space
+            if room_type == 'livingspace':
+                # check if room already exists
+                if room_type not in dojo_livingspaces:
+                    # loop through range in living space to create multiple rooms
+                    for room in room_name:
+                        new_livingspace = LivingSpace(room)
+                        dojo_livingspaces.append(new_livingspace)
+                        print('A new living space called {} has been created'.format(room))
+                else:
+                    print('The room {} already exists'.format(room_name))
+            # else
+            else:
+                # invalid room
+                print('You have entered an invalid room')
+        # else raise value error 
+        else:
+            raise ValueError
+
+        
