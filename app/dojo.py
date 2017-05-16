373e2152c3178da683093541e45a8d650d3de322
BMNyabuto <bnyabuto@gmail.com>
2017-05-16 15:02:05 +0300

    working version of create_room

--- app/dojo.py
+++ app/dojo.py
@@ -1,50 +1,57 @@
+import random 
+from collections import defaultdict
 from office import Office
 from livingspace import LivingSpace
 
 class Dojo(object):
     """docstring for Dojo"""
-    dojo_offices = {}
-    dojo_livingspaces = {}
-    staff_and_fellows = {}
+    def __init__(self):
+
+        self.dojo_offices = {}
+        self.dojo_livingspaces = {}
+        self.staff_and_fellows = {}
 
 
-    def create_room(room_type, *room_name):        
+    def create_room(self,room_type, room_name):              
         # if isinstance(room_type, str) and isinstance(room_name, str)
-        if type(room_type) == str and type(room_name) == str:
+        # if type(room_type) == str and type(room_name) == str:
             # if room type is office
-            if room_type == 'office':
-                # check if room exists
-                if room_type not in dojo_offices:
-                    # loop through range in room name to create multiple rooms
-                    for room in room_name:
-                        new_office = Office(room)
-                        dojo_offices[new_office] = []
-                        print('A new office called {} has been created'.format(room))
-                else:
-                    print('The room {} already exists'.format(room_name))
-            # if room is living space
-            if room_type == 'livingspace':
-                # check if room already exists
-                if room_type not in dojo_livingspaces:
-                    # loop through range in living space to create multiple rooms
-                    for room in room_name:
-                        new_livingspace = LivingSpace(room)
-                        dojo_livingspaces[new_livingspace] = []
-                        print('A new living space called {} has been created'.format(room))
-                else:
-                    print('The room {} already exists'.format(room_name))
-            # else
+        if room_type == 'office':
+            # check if room exists
+            if room_name not in self.dojo_offices:
+                self.dojo_offices[room_name] = []
+                print('A new office called {} has been created'.format(room_name))
             else:
-                # invalid room
-                print('You have entered an invalid room')
-        # else raise value error 
+                print('The room {} already exists'.format(room_name))
+        # if room is living space
+        elif room_name == 'livingspace':
+            # check if room already exists
+            if room_type not in self.dojo_livingspaces:
+                self.dojo_livingspaces[room_name] = []
+                print('A new living space called {} has been created'.format(room))
+            else:
+                print('The room {} already exists'.format(room_name))
+        # else
         else:
-            raise ValueError
+            # invalid room
+            print('You have entered an invalid room')
+        # else raise value error 
+        # else:
+            # raise ValueError
 
 
-        def add_person(name, position, wants_accomodation):
-            # if name is a string
-                # adds a person to the system
-                # allocates to a random room
-
-        
+    def add_person(self, name, position, wants_accomodation):
+        if position == 'STAFF':
+            self.staff_and_fellows[name] = 'staff'
+            print('Staff {} has been succesfully added'.format(name))
+            # adds a person to the system
+            # allocates to a random room
+            def allocate_random_room():
+                random_room = random.choice(list(self.dojo_offices))
+                if self.dojo_offices
+                self.dojo_offices[random_room].append(name)
+                print(self.dojo_offices)
+                print(name)
+        allocate_random_room()
+                
+            
