2239e44e9c9607d09d703054b99d4104e8515254
BMNyabuto <bnyabuto@gmail.com>
2017-05-17 16:18:08 +0300

    add print allocations. Tested and working

--- app/dojo.py
+++ app/dojo.py
@@ -3,14 +3,19 @@ import sys
 from collections import defaultdict
 from office import Office
 from livingspace import LivingSpace
+from fellow import Fellow
+from staff import Staff
 
 class Dojo(object):
     """docstring for Dojo"""
+
     def __init__(self):
 
         self.dojo_offices = {}
         self.dojo_livingspaces = {}
         self.staff_and_fellows = {}
+
+        
 
 
     def create_room(self,room_type, room_name):              
@@ -22,10 +27,11 @@ class Dojo(object):
             if room_name not in self.dojo_offices:
                 # create an office
                 new_office = Office(room_name)
-                self.dojo_offices[new_office] = []
+                self.dojo_offices[room_name] = []
                 print('A new office called {} has been created'.format(room_name))
             else:
                 print('The room {} already exists'.format(room_name))
+
         # if room is living space
         elif room_name == 'livingspace':
             # check if room already exists
@@ -42,97 +48,114 @@ class Dojo(object):
         # else raise value error 
         # else:
             # raise ValueError
+        
+
+    def allocate_random_office(self,name):
+        random_room = random.choice(list(self.dojo_offices))
+        if (len(self.dojo_offices[random_room])) < Office.max_people:
+            self.dojo_offices[random_room].append(name)
+            print('{} has been allocated the office {}'.format(name,random_room))
+        else:
+            self.allocate_random_office()
+
+    def allocate_random_livingspace(self,name):
+        random_room = random.choice(list(self.dojo_livingspaces))
+        if (len(self.dojo_livingspaces[random_room])) < LivingSpace.max_people:
+            self.dojo_livingspaces[random_room].append(name)
+            print('{} has been allocated the living space {}'.format(name,random_room))
+        else:
+            self.allocate_random_room()
+
+
 
 
     def add_person(self, name, position, wants_accomodation):
         office ='office'
         livingspace = 'Living space'
-        # A function that allocates a random room
-        def allocate_random_room(room_dictionary, type_of_room, max_num_of_people):
-            random_room = random.choice(list(room_dictionary))
-            if (len(room_dictionary[random_room])) < max_num_of_people:
-                room_dictionary[random_room].append(name)
-                print('{} has been allocated the {} {}'.format(name,type_of_room, random_room))
-            else:
-                allocate_random_room(room_dictionary,type_of_room,max_num_of_people)
+        # A function that allocates a random room        
 
-        if (position == 'STAFF') and (wants_accomodation == 'N'):
+        if (position == 'STAFF'):
             # adds a person to the system
             new_staff = Staff(name)
-            self.staff_and_fellows[new_staff] = position            
+            self.staff_and_fellows[name] = position            
             # allocate staff a random room
-            allocate_random_room(self.dojo_offices,office, Office('Office').max_people)
-
-        elif (position == 'STAFF') and (wants_accomodation == 'Y'):
-            print('Sorry there are no living spaces available for staff')
-
-        elif (position == 'FELLOW') and (wants_accomodation == 'N'):
-            # Adds fellow to the system
-            new_fellow = Fellow(name)
-            self.staff_and_fellows[new_fellow] = position
-            # Allocate the fellow a random room
-            allocate_random_room(self.dojo_offices,office, Office.max_people)        
+            self.allocate_random_office(name)
+            if (wants_accomodation == 'Y'):
+                print('Sorry there are no living spaces available for staff')            
 
         else:
             # Adds fellow to the system
             new_fellow = Fellow(name)
-            self.staff_and_fellows[new_fellow] = position
+            self.staff_and_fellows[name] = position
             # Allocate the fellow a random room
-            allocate_random_room(self.dojo_offices,office, Office.max_people)
-            allocate_random_room(self.dojo_livingspaces,livingspace, LivingSpace.max_people)
+            self.allocate_random_office(name)
+            if (wants_accomodation == 'Y'):
+                self.allocate_random_livingspace(name)
 
-
-    def print_name(self, name):
-        def print_out(which_dictionary):                
-                # print the key as header
-                print(name)
-                # print the decoration
-                print('__________________________________________________')
-                # print print the names in the list seperated by a comma
-                for names in which_dictionary[name]:
-                    print(names, end=', ')
+      
+    def print_name(self, name):        
         # check if name is in offices
+
         if name in self.dojo_offices:
-            print_out(self.dojo_offices)
+            # print the key as header
+            print(name)
+            # print the decoration
+            print('__________________________________________________')
+            # print print the names in the list seperated by a comma
+            for names in self.dojo_offices[name]:
+                print(names, )
+
         elif name in self.dojo_livingspaces:
-            print_out(self.dojo_livingspaces)
+            # print the key as header
+            print(name)
+            # print the decoration
+            print('__________________________________________________')
+            # print print the names in the list seperated by a comma
+            for names in self.dojo_livingspaces[name]:
+                print(names, )
+            
         else:
             print('Sorry the room does not exist')
+    def rooms_allocation(self,type_of_room):
+        # get a list of all the keys
+        
+        print_allocations_list = []
+        for key, value in type_of_room.iteritems():
+            print_allocations_list.append(key)
+        # for every key iterate through and print out allocations
+        for i in range (len(print_allocations_list)):
+            print('\n')
+            print(print_allocations_list[i])
+
+            print('____________________________________________________\n')
+            for names in type_of_room[print_allocations_list[i]]:
+                print names , 
+            print('\n')
 
 
-    def print_allocations(self,output):
-        def rooms_allocation(room_dictionary):
-            # get a list of all the keys
-            print_allocations_list = []
-            for key, value in room_dictionary:
-                print_allocations_list.append(key)
-            # for every key iterate through and print out allocations
-            for i in range len(print_allocations_list):
-                print(room_dictionary[print_allocations_list[i]])
-                print('____________________________________________________')
-                for names in room_dictionary[print_allocations_list[i]]:
-                    print(names, end=', ')
-        if output == 'NO':
-            print('_____________OFFICE ALLOCATION_______________')
-            rooms_allocation(self.dojo_offices)
-            print('_____________LIVING SPACE ALLOCATION_________')
-            rooms_allocation(self.dojo_livingspaces)
 
-        else:
+    def print_allocations(self):
 
-            orig_stdout = sys.stdout
-            saveFile = open('Print_allocations.txt', 'w')
-            sys.stdout = saveFile
+        print('_____________OFFICE ALLOCATION_______________\n')
+        self.rooms_allocation(self.dojo_offices)
+        print('_____________LIVING SPACE ALLOCATION_________\n')
+        self.rooms_allocation(self.dojo_livingspaces)
+
+        # else:
+
+        #     orig_stdout = sys.stdout
+        #     saveFile = open('Print_allocations.txt', 'w')
+        #     sys.stdout = saveFile
 
 
-            print('_____________OFFICE ALLOCATION_______________')
-            rooms_allocation(self.dojo_offices)
-            print('_____________LIVING SPACE ALLOCATION_________')
-            rooms_allocation(self.dojo_livingspaces)
+        #     print('_____________OFFICE ALLOCATION_______________')
+        #     self.rooms_allocation(dojo_offices)
+        #     print('_____________LIVING SPACE ALLOCATION_________')
+        #     self.rooms_allocation(dojo_livingspaces)
             
 
-            sys.stdout = orig_stdout
-            saveFile.close()
+        #     sys.stdout = orig_stdout
+        #     saveFile.close()
 
         
 
