c92196bfacde6b24ad69c5b116796d0b17f12e8b
charlieoduk <odukjr@gmail.com>
2017-06-08 00:18:51 +0300

    Add the directory app.nameOfFile to work on pytest for python3

--- app/livingspace.py
+++ app/livingspace.py
@@ -1,4 +1,4 @@
-from room import Room
+from app.room import Room
 
 class LivingSpace(Room):
     max_people = 4
--- app/office.py
+++ app/office.py
@@ -1,4 +1,4 @@
-from room import Room
+from app.room import Room
 
 
 class Office(Room):
